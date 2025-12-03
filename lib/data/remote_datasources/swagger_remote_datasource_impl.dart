336fd4609657d6aec8fededd604b696022c5b54b
Marjan DavoodiNejad <marjan.dn96@yahoo.com>
2025-12-03 20:22:29 +0330

    test(data): implement SwaggerRemoteDatasource failed state and happy path test

--- lib/data/remote_datasources/swagger_remote_datasource_impl.dart
+++ lib/data/remote_datasources/swagger_remote_datasource_impl.dart
@@ -4,6 +4,8 @@ import 'package:mock_api_generator/data/models/swagger_json_response.dart';
 import 'package:mock_api_generator/data/remote_datasources/swagger_remote_datasource.dart';
 import 'package:http/http.dart' as http;
 
+import '../../core/exception/exceptions.dart';
+
 class SwaggerRemoteDatasourceImpl implements SwaggerRemoteDataSource {
   final http.Client _client;
 
@@ -11,8 +13,20 @@ class SwaggerRemoteDatasourceImpl implements SwaggerRemoteDataSource {
 
   @override
   Future<SwaggerJsonResponse> getSwaggerLinkJsonData(link) async {
-    final result = await _client.get(Uri.parse(link));
+    try {
+      final result = await _client.get(Uri.parse(link));
 
-    return SwaggerJsonResponse.fromJson(jsonDecode(result.body));
+      if (result.statusCode == 200) {
+        return SwaggerJsonResponse.fromJson(jsonDecode(result.body));
+      } else if (result.statusCode == 400) {
+        throw RequestExceptions(message: 'Invalid link');
+      } else {
+        final m = result.body.toString();
+
+        throw JsonParsingException(message: m);
+      }
+    } catch (e) {
+      throw JsonParsingException(message: e.toString());
+    }
   }
 }
--- test/data/remote_datasource/swagger_remote_datasource_test.dart
+++ test/data/remote_datasource/swagger_remote_datasource_test.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+import 'package:mock_api_generator/core/exception/exceptions.dart';
 import 'package:mock_api_generator/data/remote_datasources/swagger_remote_datasource_impl.dart';
 
 main() {
@@ -17,5 +18,29 @@ main() {
     final result = await swagerRemoteDataSource.getSwaggerLinkJsonData(validLink);
 
     expect(result.openapi, "3.0.1");
+  });
+
+  test('should throw ParseException when given invalid JSON data', () async {
+    const errorMessage = 'Something went wrong';
+    final mockClient = MockClient((request) async => Response('Something went wrong', 500));
+
+    try {
+      SwaggerRemoteDatasourceImpl(mockClient);
+    } catch (e) {
+      expect(e, isA<JsonParsingException>().having((e) => e.message, 'message', equals(errorMessage)));
+      expect((e as JsonParsingException).message, equals(errorMessage));
+    }
+  });
+
+  test('should throw RequestExceptions when given invalid link', () async {
+    const errorMessage = 'Invalid link';
+    final mockClient = MockClient((request) async => Response('Invalid link', 400));
+
+    try {
+      SwaggerRemoteDatasourceImpl(mockClient);
+    } catch (e) {
+      expect(e, isA<RequestExceptions>().having((e) => e.message, 'message', equals(errorMessage)));
+      expect((e as RequestExceptions).message, equals(errorMessage));
+    }
   });
 }
